cad880a76a2ac8f619cacfb4c490769279128014
anahenna <anahennasanchezg@gmail.com>
2024-02-17 17:19:27 -0300

    tooltiplisto

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+$(document).ready(function () {
+    let nombrejq = $("#nombre")
+    let asuntojq = $("#asunto")
+    let mensajejq = $("#mensaje")
+
+})
